afcb14a8194ab181ac31115e9c41212f20abe0c3
Adrianna <ajelesnianska@civisanalytics.com>
2015-12-22 10:59:56 -0600

    BUG: rom to from and new line

--- src/accept_job.py
+++ src/accept_job.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pythonf
-rom __future__ import print_function 
+from __future__ import print_function 
 from gnip_historical.gnip_historical_cmd import GnipHistoricalCmd
 
 class AcceptJob(GnipHistoricalCmd):
